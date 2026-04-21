debeb608a01d601ee583b0b4aa962b85865b2a3c
rakshitkumarjha26-a11y <rakshitkumarjha26@gmail.com>
2026-04-21 10:04:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def get_db():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='pft@2009',            # ← YOUR MYSQL PASSWORD HERE
+        password='',            # ← YOUR MYSQL PASSWORD HERE
         database='cloudvault'
     )
 
